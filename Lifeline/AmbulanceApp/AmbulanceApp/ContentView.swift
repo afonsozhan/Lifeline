8ba165de89bea8e29c6d1697544b8b43e663b7a4
brandon-zhou <57579343+brandon-zhou@users.noreply.github.com>
2021-10-23 14:23:24 -0400

    some stuff

--- Lifeline/AmbulanceApp/AmbulanceApp/ContentView.swift
+++ Lifeline/AmbulanceApp/AmbulanceApp/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
     @ObservedObject private var locationManager = LocationManager()
     @State private var region = MKCoordinateRegion.defaultRegion
     @State private var cancellable: AnyCancellable?
+    @State var NumberToMessage = ""
+    @State var Message = ""
     
     private func setCurrentLocation() {
         cancellable = locationManager.$location.sink { location in
@@ -26,19 +28,41 @@ struct ContentView: View {
         }
     }
     
+    
     var body: some View {
         
         VStack {
             if locationManager.location != nil {
-                Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: nil).frame(width: screenWidth, height: screenWidth * 1.75).padding(.bottom, screenWidth * 0.5)
+                Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: nil).frame(width: screenWidth, height: screenWidth * 1.75).padding(.bottom, screenWidth * 0.2)
+                VStack{
+                    TextField("Enter phone number here here", text: $NumberToMessage).padding(.bottom)
+                    TextField("Enter your message here", text: $Message).padding(.bottom)
+                }
+                Button(action: {
+                    sendMessage()
+                }, label: {
+                Text("Send Message")
+                        .padding(.bottom, screenWidth * 0.5)
+                })
             } else {
                 Text("Locating user location...")
             }
+
+
         }
         
         .onAppear {
             setCurrentLocation()
         }
+
+       
+    }
+    
+    func sendMessage(){
+        let sms: String = "sms+14046301024&body=hello"
+        let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        //let strURL = "https://www.google.com"
+        UIApplication.shared.open(URL.init(string: strURL)!, options: [:], completionHandler: nil)
     }
 }
 
